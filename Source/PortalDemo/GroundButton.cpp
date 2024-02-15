90a95933be55322731113b5f81e51e876c7ce00d
Filip <51928186+FilipQni@users.noreply.github.com>
2024-02-15 19:29:59 +0100

    Add GroundButton class

---
+++ Source/PortalDemo/GroundButton.cpp
@@ -0,0 +1,95 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GroundButton.h"
+
+#include "InteractInterface.h"
+#include "Components/BoxComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values
+AGroundButton::AGroundButton()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	SetRootComponent(Root);
+
+	MeshButton = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Button Mesh"));
+	MeshButton->SetupAttachment(Root);
+
+	MeshCover = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Cover Mesh"));
+	MeshCover->SetupAttachment(Root);
+
+	TriggerZone = CreateDefaultSubobject<UBoxComponent>(TEXT("Trigger zone"));
+	TriggerZone->SetupAttachment(MeshButton);
+}
+
+// Called when the game starts or when spawned
+void AGroundButton::BeginPlay()
+{
+	Super::BeginPlay();
+
+	TArray<AActor*> FoundActors;
+	UGameplayStatics::GetAllActorsWithInterface(GetWorld(), UInteractInterface::StaticClass(), FoundActors);
+
+	AInteractive* FoundActorInteractive;
+	for (AActor* FoundActor : FoundActors)
+	{
+		FoundActorInteractive = Cast<AInteractive>(FoundActor);
+		if (FoundActorInteractive->GetInteractionId() == this->InteractionId)
+		{
+			ObjectToInteract = FoundActorInteractive;
+		}
+	}
+
+	if (ObjectToInteract == nullptr)
+		UE_LOG(LogTemp, Error, TEXT("Ground button has no %s Object to Interact"), *this->GetName());
+
+	TriggerZone->OnComponentBeginOverlap.AddDynamic(this, &AGroundButton::OnOverlapBegin);
+	TriggerZone->OnComponentEndOverlap.AddDynamic(this, &AGroundButton::OnOverlapEnd);
+
+	NotPressedButtonPosition = MeshButton->GetRelativeLocation().Z;
+	PressedButtonPosition = NotPressedButtonPosition - 10;
+}
+
+// Called every frame
+void AGroundButton::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (IsOverlapped)
+	{
+		if (MeshButton->GetRelativeLocation().Z > PressedButtonPosition)
+		{
+			CurrentLocation = MeshButton->GetRelativeLocation();
+			CurrentLocation.Z -= 1;
+			MeshButton->SetRelativeLocation(CurrentLocation);
+		}
+	}
+	else
+	{
+		if (MeshButton->GetRelativeLocation().Z < NotPressedButtonPosition)
+		{
+			CurrentLocation = MeshButton->GetRelativeLocation();
+			CurrentLocation.Z += 1;
+			MeshButton->SetRelativeLocation(CurrentLocation);
+		}
+	}
+}
+
+void AGroundButton::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+                                   UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+                                   const FHitResult& SweepResult)
+{
+	IsOverlapped = true;
+	ObjectToInteract->ReactToInteraction();
+}
+
+void AGroundButton::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+                                 UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	IsOverlapped = false;
+	ObjectToInteract->ReactToInteraction();
+}
---
+++ Source/PortalDemo/GroundButton.h
@@ -0,0 +1,55 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Interactive.h"
+#include "GameFramework/Actor.h"
+#include "GroundButton.generated.h"
+
+class IInteractInterface;
+class UBoxComponent;
+
+UCLASS()
+class PORTALDEMO_API AGroundButton : public AInteractive
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AGroundButton();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	UPROPERTY(VisibleAnywhere, Category="Mesh")
+	USceneComponent* Root;
+
+	UPROPERTY(VisibleAnywhere, Category="Mesh")
+	UStaticMeshComponent* MeshButton;
+
+	UPROPERTY(VisibleAnywhere, Category="Mesh")
+	UStaticMeshComponent* MeshCover;
+
+	UPROPERTY(VisibleAnywhere, Category="Mesh")
+	UBoxComponent* TriggerZone;
+
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+								  int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+	
+	AInteractive* ObjectToInteract;
+	FVector CurrentLocation;
+	bool IsOverlapped = false;
+	double PressedButtonPosition; 
+	double NotPressedButtonPosition;
+};
