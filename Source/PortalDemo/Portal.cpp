347603145a3e7e78a5440e7966a581dcbaf60995
Filip <51928186+FilipQni@users.noreply.github.com>
2024-02-15 19:31:30 +0100

    Remove UE_LOG in Portal class

--- Source/PortalDemo/Portal.cpp
+++ Source/PortalDemo/Portal.cpp
@@ -91,10 +91,10 @@ FVector APortal::GetDefaultScreenCaptureLocation() const
 void APortal::OnActorBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
                                   int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
+
 	if(Active && OtherActor->GetName() == "BP_PlayerCharacter_C_0")
 	{
 		OnOverlapDelegate.ExecuteIfBound(OtherActor);
-		//UE_LOG(LogTemp, Warning, TEXT("OtherActor: %s"), *OtherActor->GetName());
 	}
 	
 }
